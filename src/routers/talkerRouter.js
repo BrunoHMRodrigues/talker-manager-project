e11317dc85afaa466480db0f04a10943c60d01d7
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-04-14 16:17:42 -0300

    Feature: Requisite 02 completed

--- src/routers/talkerRouter.js
+++ src/routers/talkerRouter.js
@@ -17,4 +17,11 @@ talkerRouter.get('/', async (req, res) => {
   return res.status(200).json(talkers);
 })
 
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  if(!(id in talkers)) res.status(404).json({message: 'Pessoa palestrante não encontrada'})
+  return res.status(200).json(talkers[id-1]);
+})
+
 module.exports = talkerRouter;
